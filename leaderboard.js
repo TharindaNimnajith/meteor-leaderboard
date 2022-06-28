9bead880f9346b2625b509ec4e3688910438ef5e
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-06-28 17:28:12 +0530

    changes

--- leaderboard.js
+++ leaderboard.js
@@ -1,8 +1,10 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from "meteor/templating";
-import { Mongo } from "meteor/mongo";
-import { Session } from "meteor/session";
-import { Random } from "meteor/random";
+// noinspection JSUnresolvedVariable
+
+import {Meteor} from 'meteor/meteor';
+import {Template} from "meteor/templating";
+import {Mongo} from "meteor/mongo";
+import {Session} from "meteor/session";
+import {Random} from "meteor/random";
 
 // Set up a collection to contain player information. On the server,
 // it is backed by a MongoDB collection named "players".
@@ -12,7 +14,7 @@ const Players = new Mongo.Collection("players");
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
+      return Players.find({}, {sort: {score: -1, name: 1}});
     },
     selectedName: function () {
       const player = Players.findOne(Session.get("selectedPlayer"));
@@ -44,7 +46,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
       const names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+        "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
       names.forEach(function (name) {
         Players.insert({
           name: name,
